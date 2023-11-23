e8b1655bd7995e413adca1aec2dd3f0d9aa0ef0f
Scott Ferguson <scottferg@outlook.com>
2023-11-22 22:47:38 -0800

    changes to comm packet structure

--- include/Packet.h
+++ include/Packet.h
@@ -3,25 +3,74 @@
 
 #include <Arduino.h>
 
-enum Entities
+enum Entities : uint8_t
 {
+    Entities_None,
     Entities_LeftMotor,
     Entities_RightMotor,
     Entities_RearMotor,
-    Entities_AllMotors,
 };
 
-enum MotorProperties
+const Entities firstEntity = Entities_LeftMotor;
+const Entities  lastEntity = Entities_RearMotor;
+
+// prefix ++
+inline Entities& operator++(Entities& k)
 {
-    MotorProperties_Goal,
-    MotorProperties_RPM,
-    MotorProperties_Power,
-    MotorProperties_DirectDrive,
+    k = (Entities)((int)k + 1);
+    return k;
+}
+
+// postfix ++
+inline Entities operator++(Entities& k, int)
+{
+    Entities o = k;
+    k = (Entities)((int)k + 1);
+    return o;
+}
+
+enum Properties : uint8_t
+{
+    Properties_None,
+    Properties_Goal,
+    Properties_RPM,
+    Properties_Power,
+    Properties_DirectDrive,
+    // controller-only properties:
+    Properties_ControlMode,
 };
+
+const Properties firstProperty = Properties_Goal;
+const Properties lastProperty = Properties_DirectDrive;
+
+// prefix ++
+inline Properties& operator++(Properties& k)
+{
+    k = (Properties)((int)k + 1);
+    return k;
+}
+
+// postfix ++
+inline Properties operator++(Properties& k, int)
+{
+    Properties o = k;
+    k = (Properties)((int)k + 1);
+    return o;
+}
 
 class Entity
 {
-    void SetProperty(int8_t property, int8_t value);
+public:
+    virtual void setProperty(int8_t property, int16_t value) = 0;
+    virtual int16_t getProperty(int8_t property) = 0;
+    virtual bool getPropertyChanged(int8_t property) = 0;
+};
+
+struct Packet
+{
+    Entities entity;
+    Properties property;
+    int16_t value;
 };
 
 #endif  // _PACKET_H
